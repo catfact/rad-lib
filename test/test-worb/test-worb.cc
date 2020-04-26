7fae4f660888facedd019c63c4a0ee1433ad2c57
ezra buchla <emb@catfact.net>
2020-04-26 13:41:07 -0700

    add worb test

---
+++ test/test-worb/test-worb.cc
@@ -0,0 +1,66 @@
+
+#include <fstream>
+#include <iostream>
+#include <sstream>
+
+#include "json.hpp"
+#include "worb.h"
+
+
+using nlohmann::json;
+
+using std::string;
+using std::stringstream;
+using std::ifstream;
+using std::cout;
+using std::cerr;
+using std::endl;
+
+int main(int argc, const char **argv){    
+    if (argc < 2) {
+	std::cerr << "missing required argument (configuration file)" << endl;
+	exit(1);
+    }
+
+    string path(argv[1]);
+
+    
+    ifstream ifs(path);
+    if (!ifs.is_open()) {
+        cerr << "failed to open config.json file: " << path << endl;
+        exit(1);
+    }
+    stringstream ss;
+    ss << ifs.rdbuf();
+    string str = ss.str();
+    cout << str << endl;
+
+
+    double period, gamma, base, noise, initial;
+    
+    try {
+        json jsn = json::parse(str);
+	period = jsn["period"];
+	gamma = jsn["gamma"];
+	base = jsn["base"];
+	noise = jsn["noise"];
+	initial = jsn["initial"];
+    } catch (json::exception &ex) {
+        cerr << "failed to parse json from file: " << path << " (" << ex.what() << ")" << endl;
+        exit(1);
+    }
+
+    rad::Worb worb;
+    worb.init(initial, base, noise);
+    worb.setPeriod(static_cast<unsigned int>(period));
+    worb.setGamma(gamma);
+
+    int n = 100;
+
+    cout << "output = [ " << endl;
+    for (int i=0; i<n; ++i) {
+	std::cout << worb.next() << ", ";
+    }
+    cout << endl << "]" << endl;
+    exit(0);    
+}
